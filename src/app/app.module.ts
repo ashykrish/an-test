e0d4fa6716553689c2d43eb921e5025cacdd28f1
Ashok Kumar <ashytecho@gmail.com>
2021-05-08 18:51:09 +0530

    integrate the API

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { HttpClient, HttpHandler } from '@angular/common/http';
-import {HttpInterceptorService} from 'src/app/services/http-interceptor/http-interceptor.service'
+import { HttpInterceptorService} from 'src/app/services/http-interceptor/http-interceptor.service'
 import { HeaderComponent } from './theme/static/header/header.component';
 import { FooterComponent } from './theme/static/footer/footer.component';
 import { LoaderComponent } from './shared/loader/loader.component';
