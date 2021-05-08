d435e74cc605aa8471ef53a32cbe8e76265bdaec
Ashok Kumar <ashytecho@gmail.com>
2021-05-08 16:35:16 +0530

    env change

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  apiBase:'http://api.openweathermap.org/data/2.5/weather?q=',
+  version:'',
+  appId:'3d8b309701a13f65b660fa2c64cdc517'
 };
