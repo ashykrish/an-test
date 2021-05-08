95266dbdfa365a63bb4f9c1826d8675e13c580ae
Ashok Kumar <ashytecho@gmail.com>
2021-05-08 18:51:44 +0530

    integrate the API

--- src/app/theme/home/detail/detail.component.ts
+++ src/app/theme/home/detail/detail.component.ts
@@ -11,9 +11,42 @@ import {environment} from 'src/environments/environment';
 export class DetailComponent implements OnInit {
   city:string;
   countryCode:string;
+  data = {
+    list:[{
+      "dt": 1620486000,
+      "main": {
+        "temp": 287.86,
+        "feels_like": 287.55,
+        "temp_min": 287.86,
+        "temp_max": 290.3,
+        "pressure": 1004,
+        "sea_level": 1004,
+        "grnd_level": 999,
+        "humidity": 83,
+        "temp_kf": -2.44
+      },
+      "dt_txt": "2021-05-08 15:00:00"
+    },
+    {
+    "dt": 1620486000,
+    "main": {
+      "temp": 287.86,
+      "feels_like": 287.55,
+      "temp_min": 287.86,
+      "temp_max": 290.3,
+      "pressure": 1004,
+      "sea_level": 1004,
+      "grnd_level": 999,
+      "humidity": 83,
+      "temp_kf": -2.44
+    },
+    "dt_txt": "2021-05-08 15:00:00"
+  }
+]
+  };
   constructor(  
     private readonly activatedRoute:ActivatedRoute, 
-    private readonly httpService: HttpService) {
+    private readonly httpService: HttpService) {      
       this.city = this.activatedRoute.snapshot.params.city;
       this.countryCode = this.activatedRoute.snapshot.params.ccode;
   }
@@ -21,11 +54,9 @@ export class DetailComponent implements OnInit {
     this.getCityDetails( this.city,this.countryCode );
   }
   getCityDetails(city:string,countryCode:string): void{
-    const url = `${this.city},${this.countryCode},&appid=${environment.appId}`;
-    console.log("url",url);
+    const url = `forecast?q=${this.city},${this.countryCode},&appid=${environment.appId}`;  
     this.httpService.doGet(url).subscribe((result: any) => {
-      console.log("result",result);
-
+     this.data = result;
     },
     (error: any) => { 
     }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiBase:'http://api.openweathermap.org/data/2.5/weather?q=',
+  apiBase:'https://api.openweathermap.org/data/2.5/',
   version:'',
   appId:'3d8b309701a13f65b660fa2c64cdc517'
 };
